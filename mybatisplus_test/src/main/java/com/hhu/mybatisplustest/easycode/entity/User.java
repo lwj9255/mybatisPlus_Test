c213d27b2a3094cc5d564e96b5c0024bc8f776f1
“lwj9255” <lwj1059168506@gmail.com>
2024-11-05 17:41:33 +0800

    mybatisplus的测试：easycode

---
+++ mybatisplus_test/src/main/java/com/hhu/mybatisplustest/easycode/entity/User.java
@@ -0,0 +1,76 @@
+package com.hhu.mybatisplustest.easycode.entity;
+
+import java.io.Serializable;
+
+/**
+ * (User)实体类
+ *
+ * @author seeasheep
+ * @since 2024-11-05 17:40:58
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 407581993668557502L;
+    /**
+     * 主键ID
+     */
+    private Long id;
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 年龄
+     */
+    private Integer age;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 是否删除
+     */
+    private Integer isDeleted;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+}
+
---
+++ mybatisplus_test/src/main/java/com/hhu/mybatisplustest/easycode/service/UserService.java
@@ -0,0 +1,56 @@
+package com.hhu.mybatisplustest.easycode.service;
+
+import com.hhu.mybatisplustest.easycode.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * (User)表服务接口
+ *
+ * @author seeasheep
+ * @since 2024-11-05 17:41:00
+ */
+public interface UserService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    User queryById(Long id);
+
+    /**
+     * 分页查询
+     *
+     * @param user 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    Page<User> queryByPage(User user, PageRequest pageRequest);
+
+    /**
+     * 新增数据
+     *
+     * @param user 实例对象
+     * @return 实例对象
+     */
+    User insert(User user);
+
+    /**
+     * 修改数据
+     *
+     * @param user 实例对象
+     * @return 实例对象
+     */
+    User update(User user);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
